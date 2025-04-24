002646bf1e66bddf27c090d90731f4fb32032d33
shahmeerfakhar <94310710+sheikh-shahab@users.noreply.github.com>
2025-04-25 01:12:32 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 
 
-import Image from "next/image";
+
 import Hero from "./components/Hero";
 import Newest from "./components/Newest";
 import Fashion from "./components/Fashion";
@@ -8,9 +8,9 @@ import Accessories from "./components/Accessories";
 import Desgin from "./components/Desgin";
 import New from "./components/New";
 import Footer from "./components/Footer";
-import Link from "next/link";
-import Navbar from "./components/Navbar";
-import { useState } from "react";
+
+
+
 
 export default function Home() {
   // const [category, setCategory] = useState("All")
